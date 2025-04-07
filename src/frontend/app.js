340b7e9d86511e78fdcd39dbdea9848aeac13e58
LeonBergmannEMF <135614260+LeonBergmannEMF@users.noreply.github.com>
2025-04-07 14:13:35 +0200

    apiendpoint

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "https://baqtestappbackend.blackglacier-c7b0844e.northeurope.azurecontainerapps.io/api/tasks";
+const apiEndpoint = "https://ca-chy-300233-239-leonbergmann.gentleflower-6e2db1e8.northeurope.azurecontainerapps.io/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
